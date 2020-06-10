71e462152ce05ebf1ece7c026ace2e1a96fbc03b
Warlock <internalmike@gmail.com>
2020-06-10 22:23:13 +0300

    fmt_lifetime

--- src/lib.rs
+++ src/lib.rs
@@ -166,4 +166,19 @@ mod tests {
         let len = writer.len;
         assert_eq!("{1}x{", &buf[.. len]);
     }
+
+    #[test]
+    fn fmt_lifetime() {
+        fn display<'a, 'b>(f: &'a str, i: &'a [u8], buf: &'b mut str) -> &'b str {
+            let args_format = dyn_fmt::Arguments::new(f, i);
+            let mut writer = Writer { buf, len: 0 };
+            write!(&mut writer, "{}", args_format).unwrap();
+            let len = writer.len;
+            &buf[.. len]
+        }
+        let mut buf = [0u8; 128];
+        let buf = str::from_utf8_mut(&mut buf).unwrap();
+        let res = display("{}", &[0], buf);
+        assert_eq!("0", res);
+    }
 }
